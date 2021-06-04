db41bf7c7853348597f7d3f2c975bd7a9fc84821
Senju007 <sitrakaphilippe@gmail.com>
2021-06-04 12:27:44 +0200

    ff

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/Home';
+import {Contants} from 'expo-constants';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Home/>
     </View>
   );
 }
@@ -17,5 +19,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop:Contants.statusBarHeight
   },
 });
---
+++ screens/home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+function Home() {
+    return (
+        <div>
+            <Text> Accueil </Text>
+        </div>
+    )
+}
+
+export default Home
+
+
+
+
